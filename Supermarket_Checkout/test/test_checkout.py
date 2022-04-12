33926890abc37c0f5c642bca21c368533cab7c0f
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-12 17:30:00 +0100

    refactoring

--- Supermarket_Checkout/test/test_checkout.py
+++ Supermarket_Checkout/test/test_checkout.py
@@ -1,13 +1,19 @@
+import pytest
 from lib.Checkout import Checkout
 
 
-# Add item price
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
 
-def test_CanAddItemPrice():
-    co = Checkout()
-    co.addItemPrice("a", 1)
+
+# Add item price
+def test_CanAddItemPrice(checkout):
+
+    checkout.addItemPrice("a", 1)
 
 # Add Item
-def test_CanAddItem():
-    co = Checkout()
-    co.addItem("a")
+def test_CanAddItem(checkout):
+
+    checkout.addItem("a")
