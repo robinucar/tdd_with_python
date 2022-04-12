89f033fc49c1654f7ea55569ee7d18c5994ab016
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-12 14:48:56 +0100

    failled test return 1 with argument 1

--- FizzBuzz/test/test_fizzBuzz.py
+++ FizzBuzz/test/test_fizzBuzz.py
@@ -2,3 +2,7 @@ import pytest
 from lib.fizzBuzz import fizzBuzz
 def test_canCallFizzBuzz():
     fizzBuzz(1)
+
+def test_returns1With1PassedIn():
+    retVal = fizzBuzz(1)
+    assert retVal == '1'
