0a4b2a2eeac2d5bad4de93eb412b498799cb0b6e
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-12 15:37:05 +0100

    Updated Readme.md, added comment

--- FizzBuzz/test/test_fizzBuzz.py
+++ FizzBuzz/test/test_fizzBuzz.py
@@ -2,24 +2,28 @@ import pytest
 from lib.fizzBuzz import fizzBuzz
 from lib.fizzBuzz import checkFizzBuzz
 
+# returns string 1 if number is 1
 
 def test_returns1With1PassedIn():
     checkFizzBuzz(1, "1")
 
+# returns string 2 if number is 2
 
 def test_returns2With2PassedIn():
     checkFizzBuzz(2, "2")
 
+# returns Fizz if number is 3
 
 def test_returnsFizzWith3PassedIn():
     checkFizzBuzz(3, "Fizz")
+
+# returns Buzzz if number is 5
 
 
 def test_returnsBuzzWith5PassedIn():
     checkFizzBuzz(5, "Buzz")
 
 # returns Fizz with multiple of 3
-
 
 def test_returnsFizzWith6PassedIn():
     checkFizzBuzz(6, "Fizz")
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ### install the last version of Python
 ### install virtualenv
 ### install pytest
+
+## This repository I am covering basic python core concepts with following Tdd principles (RED, GREEN, REFACTORING)
+### First step:  Code Failled test (RED)
+### Second step: Update production code according to test and make test passed (Green)
+### Third step: Remove all dublication and unnecessary codes (Refactoring)
