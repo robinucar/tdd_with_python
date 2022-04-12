b872e84648e2397dcfc05fe96340a87b38daefc4
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-12 16:55:38 +0100

    add item and price test -red-

--- Supermarket_Checkout/test/test_checkout.py
+++ Supermarket_Checkout/test/test_checkout.py
@@ -2,3 +2,9 @@
 from lib.Checkout import Checkout
 def test_CanInstantiateCheckout():
     co = Checkout()
+
+# Add item price
+
+def test_CanAddItemPrice():
+    co = Checkout()
+    co.addItemPrice("a", 1)
