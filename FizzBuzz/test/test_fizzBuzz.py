7fc087838726067fb1c291369e6bfd06db8dba6c
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-12 14:53:23 +0100

    test with argument 2 failled

--- FizzBuzz/test/test_fizzBuzz.py
+++ FizzBuzz/test/test_fizzBuzz.py
@@ -4,3 +4,7 @@ from lib.fizzBuzz import fizzBuzz
 def test_returns1With1PassedIn():
     retVal = fizzBuzz(1)
     assert retVal == '1'
+
+def test_returns2With2PassedIn():
+    retVal = fizzBuzz(2)
+    assert retVal == "2"
