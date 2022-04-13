0509e7f50537b6a0d05448480f100abd71a4bd15
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-13 20:26:13 +0100

    refactoring

--- FileReader/test/test_lineReader.py
+++ FileReader/test/test_lineReader.py
@@ -3,14 +3,18 @@ from pytest import raises
 from unittest.mock import MagicMock
 from lib.LineReader import readFromFile
 
-# readFromFile returns correct string using MagicMock
 
-
-def test_returnsCorrectString(monkeypatch):
+@pytest.fixture()
+def mock_open(monkeypatch):
     mock_file = MagicMock()
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr('builtins.open', mock_open)
+    return mock_open
+# readFromFile returns correct string using MagicMock
+
+
+def test_returnsCorrectString(mock_open, monkeypatch):
     mock_exists = MagicMock(return_value=True)
     monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile("python")
@@ -18,16 +22,9 @@ def test_returnsCorrectString(monkeypatch):
     assert result == "test line"
 
 
-
 # readFromFile throws exception when file does not exist
 def test_throwsExceptionWithNonExistFile(monkeypatch):
-    mock_file = MagicMock()
-    mock_file.readline = MagicMock(return_value="test line")
-    mock_open = MagicMock(return_value=mock_file)
-    monkeypatch.setattr('builtins.open', mock_open)
     mock_exists = MagicMock(return_value=False)
     monkeypatch.setattr("os.path.exists", mock_exists)
     with raises(Exception):
         result = readFromFile("Java")
-
-
