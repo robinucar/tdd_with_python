3a2d3aa5e4ba7d41b90f014d9150ce3dffba3741
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-13 20:15:00 +0100

    both test cases are green

--- FileReader/test/test_lineReader.py
+++ FileReader/test/test_lineReader.py
@@ -11,6 +11,8 @@ def test_returnsCorrectString(monkeypatch):
     mock_file.readline = MagicMock(return_value="test line")
     mock_open = MagicMock(return_value=mock_file)
     monkeypatch.setattr('builtins.open', mock_open)
+    mock_exists = MagicMock(return_value=True)
+    monkeypatch.setattr("os.path.exists", mock_exists)
     result = readFromFile("python")
     mock_open.assert_called_once_with("python", "r")
     assert result == "test line"
