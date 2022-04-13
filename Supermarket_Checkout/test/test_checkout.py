b078d8cfd249fa5f1b7060c7f16f53f9f82c00b6
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-13 16:44:11 +0100

    removed dublicate addItemPrice method from tests -refactoring-

--- Supermarket_Checkout/test/test_checkout.py
+++ Supermarket_Checkout/test/test_checkout.py
@@ -5,15 +5,15 @@ from lib.Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice('a', 1)
+    checkout.addItemPrice('b', 2)
+    checkout.addItemPrice('c', 3)
     return checkout
 
 
 # Calculate total
 
 def test_CanCalculateTotal(checkout):
-    # Add Item Price
-    checkout.addItemPrice("a", 1)
-
     # Add Item
     checkout.addItem("a")
 
@@ -21,15 +21,16 @@ def test_CanCalculateTotal(checkout):
     assert checkout.calculateTotal() == 1
 
 # Get Correct Total Amount With Multiple Items
+
+
 def test_GetCorrectTotalWithMultipleItems(checkout):
-        checkout.addItemPrice('a', 1)
-        checkout.addItemPrice('b', 2)
-        checkout.addItemPrice('c', 3)
-        checkout.addItem('a')
-        checkout.addItem('b')
-        checkout.addItem('c')
-        assert checkout.calculateTotal() == 6
+    checkout.addItem('a')
+    checkout.addItem('b')
+    checkout.addItem('c')
+    assert checkout.calculateTotal() == 6
 
 # Add discount
+
+
 def test_CanAddDiscountRule(checkout):
     checkout.addDiscount("a", 3, 2)
