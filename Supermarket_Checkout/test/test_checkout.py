bebe356087589ddb832dac6684e5b8cff770c0e4
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-13 10:21:10 +0100

    test calculate total -red-

--- Supermarket_Checkout/test/test_checkout.py
+++ Supermarket_Checkout/test/test_checkout.py
@@ -17,3 +17,10 @@ def test_CanAddItemPrice(checkout):
 def test_CanAddItem(checkout):
 
     checkout.addItem("a")
+
+# Calculate total
+
+def test_CanCalculateTotal(checkout):
+    checkout.addItemPrice("a", 1)
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 1
