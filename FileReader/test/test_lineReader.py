5488ea84de4b813aae48988eb5bacae720974248
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-13 19:53:16 +0100

    test readFromFile returns correct string usin MagicMock -red-

--- FileReader/test/test_lineReader.py
+++ FileReader/test/test_lineReader.py
@@ -1,5 +1,16 @@
 import pytest
+from unittest.mock import MagicMock
 from lib.LineReader import readFromFile
 
-def test_canCallReadFromFile():
-    readFromFile("python")
+# Call readFromFile
+
+
+# readFromFile returns correct string usin MagicMock
+def test_returnsCorrectString(monkeypatch):
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value="test line")
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr('builtins.open', mock_open)
+    result = readFromFile("python")
+    mock_open.assert_called_once_with("python", "r")
+    assert result == "test line"
