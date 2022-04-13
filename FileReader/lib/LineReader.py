88d144fdf63166349f4f0b43cb7d643c8412783b
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-13 20:14:18 +0100

    test readFromFile throws exception when file does not exist -green- first test case is red because of fake file name and path

--- FileReader/lib/LineReader.py
+++ FileReader/lib/LineReader.py
@@ -1,4 +1,9 @@
+import os
+
+
 def readFromFile(filename):
+    if not os.path.exists(filename):
+        raise Exception("File is not exist...")
     infile = open(filename, "r")
     line = infile.readline()
     return line
